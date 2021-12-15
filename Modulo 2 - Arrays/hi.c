f5bbaa4960fed30448455af56d35982fcd33a6e7
Willian Vasselo <94553130+Willianvass@users.noreply.github.com>
2021-12-14 23:09:06 -0300

    Add files via upload

---
+++ Modulo 2 - Arrays/hi.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string s = "HI!";
+
+    /*char c1 = 'H';//Transformando tudo em uma String
+    char c2 = 'I';
+    char c3 = '!';*/
+
+    printf("%s\n", s);
+    //printf("%i %i %i\n", c1, c2, c3);
+}
+
+
---
+++ Modulo 2 - Arrays/string.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main(void)
+{
+    string s = get_string("Input: ");
+    printf("Output: ");
+    for (int i = 0; n = strlen(s); i < n; i++)
+    {
+        printf("%c", s[i]);
+    }
+    printf("\n");
+}
+
+
